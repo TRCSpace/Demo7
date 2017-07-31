4121259940c18b4352bb7cd13326d19ec550a1a5
patrickmoniz <patrickmoniz@gmail.com>
2017-07-31 15:11:34 +0100

    combustor SCAD script generation complete

--- Combustor.py
+++ Combustor.py
@@ -1,11 +1,27 @@
+# ! /usr/bin/env python
+# -*- coding: UTF-8 -*-
+from __future__ import division
+import os
+import sys
+import re
+from solid import *
+from solid.utils import *
+
 from Component import Component
-import solid
+# Assumes SolidPython is in site-packages or elsewhwere in sys.path
 
 class Combustor(Component):
   pass
 
+SEGMENTS = 48
 
-def RenderToModel(geoms):
+def RenderToModel():
+    # Your code here!
+    combustor = cylinder(r=100, h=100)
+    combustor -= cylinder(r=80, h=100)
 
-  c = solid.cylinder(r=10, h=100)
-  c -= solid.cylinder(r=2, h=100)
+    return combustor
+
+if __name__ == '__main__':
+    combCAD = RenderToModel()
+    scad_render_to_file(combCAD, file_header='$fn = %s;' % SEGMENTS, include_orig_code=True)
