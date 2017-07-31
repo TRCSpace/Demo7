c680c0fbbcff738d132502b3477b25b97a7717fe
patrickmoniz <patrickmoniz@gmail.com>
2017-07-31 16:19:42 +0100

    Combustion Thermo Model integrated

--- Combustor.py
+++ Combustor.py
@@ -7,20 +7,22 @@ import re
 from solid import *
 from solid.utils import *
 
-import Nodes
+from pprint import pprint
 import cantera as ct
 import CoolProp as cp
 
 from Component import Component
-# Assumes SolidPython is in site-packages or elsewhwere in sys.path
+import Nodes
+
 
 class Combustor(Component):
+
     def __init__(self, nodes):
 
-        self.T_r = nodes.T_r[0]
-        self.OFR = nodes.OFR[0]
-        self.P_c = nodes.P_c[0]
-        self.mdot = nodes.mdot_t[0]
+        nodes.T_r = nodes.T_r
+        self.OFR = nodes.OFR
+        self.P_c = nodes.P_c
+        self.mdot_t = nodes.mdot_t
         self.fuel = nodes.fuel
         self.oxidiser = nodes.oxidiser
         # Beginning Cantera analysis (equilibrium combustion analysis):
@@ -28,25 +30,25 @@ class Combustor(Component):
         self.gas.transport_model = 'Multi'
         self.ct_OFR_string = self.fuel + ':1, ' + self.oxidiser + ':' + str(self.OFR)
         self.gas.Y = self.ct_OFR_string
-        self.gas.TP = self.T_r, self.P_c
+        self.gas.TP = nodes.T_r, self.P_c
         self.gas.equilibrate('HP')
         # Extracting gas properties from the gas Solution object - storing them permanently, so that ct doesn't alter
         # them when we call it in the future
         self.gamma = self.gas.cp_mass / self.gas.cv_mass
         self.cp = self.gas.cp_mass
         self.mu = self.gas.viscosity
-        self.T0 = self.gas.T
+        nodes.T0 = self.gas.T
         self.P0 = self.gas.P
         self.Mr = self.gas.mean_molecular_weight
         self.rho = self.gas.density_mass
-        self.c0 = math.sqrt(self.gamma * 8314 * self.T0 / self.Mr)
+        self.c0 = math.sqrt(self.gamma * 8314 * nodes.T0 / self.Mr)
         self.k_th = self.gas.thermal_conductivity
         self.Pr = self.cp * self.mu / self.k_th
         self.Y = self.gas.mass_fraction_dict(1e-6)
         self.X = self.gas.mole_fraction_dict(1e-6)
 
         # Placeholder variables - will be operated on by other functions:
-        self.T = self.T0
+        nodes.T = nodes.T0
         self.P = self.P0
         self.M = 0
         self.v = self.M * self.c0
@@ -58,13 +60,13 @@ class Combustor(Component):
 
         # Determining Required velocity for jet atomisation:
         # Wrap into a function
-        self.mdot_f = self.mdot_t * (1 / (self.OFR + 1))
+        self.mdot_f = nodes.mdot_t * (1 / (nodes.OFR + 1))
         # Propellant Density:
-        self.rho_f = cp.CoolProp.PropsSI('D', 'P', self.P_c, 'T', self.T_r, nodes.fuel)
+        self.rho_f = cp.CoolProp.PropsSI('D', 'P', self.P_c, 'T', nodes.T_r, nodes.fuel)
         # Propellant viscosity:
-        self.mu_f = cp.CoolProp.PropsSI('viscosity', 'P', self.P_c, 'T', self.T_r, nodes.fuel)
+        self.mu_f = cp.CoolProp.PropsSI('viscosity', 'P', self.P_c, 'T', nodes.T_r, nodes.fuel)
         # Propellant Surface tension:
-        self.sigma_f = cp.CoolProp.PropsSI('surface_tension', 'P', self.P_c, 'T', self.T_r, nodes.fuel)
+        self.sigma_f = cp.CoolProp.PropsSI('surface_tension', 'P', self.P_c, 'T', nodes.T_r, nodes.fuel)
 
         # These constants are the slope and y-intercept of the log-linear fit for the Reynolds - Ohnesorge fit for
         # instantaneous droplet atomisation conditions (bit of a mouthful!)
@@ -172,12 +174,95 @@ class Combustor(Component):
         self.E = (4 * math.pi / self.A) * (1 - (self.D * self.B))
 
         # Critical Weber number (accounting for turbulence and density effects)
-
         self.We_crit = self.E * (1 + (self.rho_amb / self.rho_f))
         # Max stable droplet diameter (theoretical)
         self.d_sbu = self.We_crit * self.sigma_f / (self.rho_amb * (self.u_jet ** 2))
         # Breakup length, based on average velocity (u_r and u_d)
         self.L_sbu = self.u_jet * self.t_sbu
+
+
+        ## Function to output combustion times
+        # Input structs: Nodes, Reactant.fuel,Pintle.output,DBU.fuel
+
+        # function[output] = ArrayFuelDropletComb(Nodes, ReactantSpecies, Combustor, PintleOutput, DBUProp)
+
+        # u_f = PintleOutput.vel_f     # Unused at present
+        # u_ox = PintleOutput.vel_ox   # Unused at present
+
+        nodes.T_prop = nodes.T_f
+        self.P_c = nodes.P_c
+
+        ## Declare Initial Conditions:
+        nodes.T_b = cp.CoolProp.PropsSI('T','P',self.P_c,'Q',0,nodes.fuel)
+        self.rho_f = cp.CoolProp.PropsSI('D', 'P', self.P_c, 'T', nodes.T_prop, nodes.fuel)
+        self.cp_f = cp.CoolProp.PropsSI('Cpmass', 'P', self.P_c, 'T', nodes.T_prop, nodes.fuel)
+        self.h_fg = abs(cp.CoolProp.PropsSI('H','P',self.P_c,'Q',1,nodes.fuel) -
+                        cp.CoolProp.PropsSI('H','P',self.P_c,'Q',0,nodes.fuel))
+        nodes.T_i = nodes.T_prop  # FUEL temperature, NOT flame temperature
+
+        nodes.T_af = nodes.T0
+        self.k_g = self.k_th
+        self.rho_g = self.rho
+        self.mu_g = self.mu
+        self.cp_g = self.cp
+
+        # u_rel = DBUProp.u_rel    # Unused at present
+        self.u_avg = self.u_rel
+        self.Pr = self.cp_g * self.mu_g / self.k_g
+
+        self.d_sbu = self.d_sbu
+
+        ## Calculating Pre-heat Time:
+        self.Re_d = self.rho_g * self.u_avg * self.d_sbu / self.mu_g
+        self.Nu = 2 + (0.4 * (self.Re_d ** 0.5) * (self.Pr ** (1 / 3)))
+        self.A = self.rho_f * self.cp_f * self.d_sbu ** 2
+        self.B= 6 * self.k_g * self.Nu
+
+        # Find a way to account for non-adiabatic effects!
+        nodes.T_amb = nodes.T_af * 1
+        # Non-dimensional temperature
+        nodes.T_nd = (nodes.T_amb - nodes.T_i) / (nodes.T_amb - nodes.T_b)
+
+        self.Dsq = self.d_sbu ** 2
+        self.dt = 1e-6
+        self.C1 = 0.5
+        self.D = self.d_sbu
+        self.C2 = 0.5
+
+        # math.log used here was the reallog function in MATLAB - ensure that results obtained here do not conflict
+        # with the MATLAB ones
+        self.t_ph = (self.A / self.B) * math.log(nodes.T_nd)
+
+        # Calculate Vaporisation Time:
+        # Method can be found in Fundamentals of Combustion Processes, McAllister, Chapter 8: Droplet Combustion
+        self.t_vap = 0  # Initialise counter variable for droplet vaporisation
+        while self.Dsq >= 0:
+            # Instantaneous droplet Reynolds number:
+            self.Re_d = self.rho_g * self.u_avg * self.D / self.mu_g
+            # Terms in theoretical formula to calculate droplet vapourisation time
+            self.beta0 = 4 * self.k_g * (nodes.T_amb - nodes.T_b) / (self.rho_f * self.h_fg * self.C1)
+            self.beta = (1.6 * self.k_g / (self.rho_f * self.h_fg)) * (nodes.T_amb - nodes.T_b) * (self.Re_d ** 0.5) * (self.Pr ** (1 / 3))
+            # d(D^2)/dt - rate of change of the droplet's diameter squared with time
+            self.dDsq_dt = -2 * (self.C1 * self.beta0) - self.beta
+            # Instantaneous square of droplet diameter
+            self.Dsq = self.Dsq + (self.dDsq_dt * self.dt)
+            # Break the while loop if there Dsq < 0, in order to stop it from throwing a math domain error
+            if self.Dsq < 0:
+                break
+            # Instantaneous droplet diameter
+            self.D = math.sqrt(self.Dsq)
+            # Counter to monitor vaporisation time
+            self.t_vap = self.t_vap + self.dt
+
+
+        # Calculate Burn Time:
+        self.beta0pr = 4 * self.k_g * (nodes.T_af - nodes.T_b) / (self.rho_f * self.h_fg * self.C2)
+        # Formula for quiescent combustion:
+        self.t_burn = (self.d_sbu ** 2) / self.beta0pr
+
+        # Calculating relevant variables
+        self.t_tot = self.t_ph + self.t_vap + self.t_burn
+        self.L_comb = self.t_tot * self.u_avg
 
 
 def RenderToModel():
@@ -188,8 +273,17 @@ def RenderToModel():
 
     return combustor
 
+
+def print_r(the_object):
+    print("CLASS: ", the_object.__class__.__name__, " (BASE CLASS: ", the_object.__class__.__bases__, ")")
+    pprint(vars(the_object))
+
+
 # Start of script:
-SEGMENTS = 48
+
+combustor = Combustor(Nodes.nodes)
+
 if __name__ == '__main__':
     combCAD = RenderToModel()
+    SEGMENTS = 48
     scad_render_to_file(combCAD, file_header='$fn = %s;' % SEGMENTS, include_orig_code=True)
