d0ce74117bc335b8aa144b89ef57f7cc3f0c2023
patrickmoniz <patrickmoniz@gmail.com>
2017-07-31 14:50:38 +0100

    Added a bit of cylinder to combustor

--- Combustor.py
+++ Combustor.py
@@ -1,4 +1,11 @@
 from Component import Component
+import solid
 
 class Combustor(Component):
   pass
+
+
+def RenderToModel(geoms):
+
+  c = solid.cylinder(r=10, h=100)
+  c -= solid.cylinder(r=2, h=100)
